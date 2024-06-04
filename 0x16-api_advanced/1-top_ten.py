4e17a90ce22c2496c23bc728879f1726e9a36bf8
brunolucy2023x <lucyb5147@gmail.com>
2024-06-04 12:24:01 +0000

    twelve

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python3
-"""Module that consumes the Reddit API and prints the titles of the first
-10 hot posts listed for a given subreddit."""
+"""
+    Uses the reddit API for get the 10 hot posts
+"""
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot
-    posts listed for a given subreddit.
-
-    If not a valid subreddit, print None.
-    Invalid subreddits may return a redirect to search results. Ensure
-    that you are not following redirects.
-
-    Args:
-        subreddit (str): subreddit
-
-    Returns:
-        str: titles of the first 10 hot posts
-    """
-    base_url = 'https://www.reddit.com'
-    sort = 'top'
-    limit = 10
-    url = '{}/r/{}/.json?sort={}&limit={}'.format(
-        base_url, subreddit, sort, limit)
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    response = requests.get(
-        url,
-        headers=headers,
-        allow_redirects=False
-    )
-    if response.status_code == 200:
-        for post in response.json()['data']['children'][0:10]:
-            print(post['data']['title'])
-    else:
+    """Get the 10 host posts"""
+    url_sred_inf = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url_sred_inf += "?limit=10"
+    headers = {'user-agent': 'request'}
+    response = requests.get(url_sred_inf, headers=headers,
+                            allow_redirects=False)
+    if str(response) != "<Response [200]>":
         print(None)
+        return
+    r_json = response.json()
+    hot_posts_json = r_json.get("data").get("children")
+    top_10_posts = ""
+    for post in hot_posts_json:
+        top_10_posts += post.get("data").get("title") + "\n"
+    print(top_10_posts, end="")
